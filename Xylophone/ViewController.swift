7e29bd0cf44d33b74434b59f145b75e2545ede11
Bogdan <41237949+c0pland@users.noreply.github.com>
2021-11-24 16:15:07 +0700

    Add files via upload
    my version of xylophone. Completed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundname: sender.currentTitle)
+        sender.alpha = 0.5
+        usleep(20000)
+        sender.alpha = 1
+    }
     
-
+    func playSound(soundname:String!) {
+        let url = Bundle.main.url(forResource: soundname, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
